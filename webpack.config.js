f9b2f07d8a0a6b75206fd9a1c6eedbe5894a33cc
张帆 <zhangfan@zhangfandeMacBook-Air.local>
2017-11-07 13:56:17 +0800

    V1.0.1

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      exclude: /ndoe_mudules/,
+      loader: 'babel-loader?presets[]=es2015&presets[]=react'
+    }, ]
+  }
+};
